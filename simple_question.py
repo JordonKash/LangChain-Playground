b0094b5bd1a020421e10ca66f9d9671e35829285
JordonKash <jordonkash@utexas.edu>
2024-02-18 14:20:51 -0800

    messing aorund with LangChain

---
+++ simple_question.py
@@ -0,0 +1,13 @@
+from langchain_openai import ChatOpenAI
+from dotenv import load_dotenv
+import os
+
+## grab my openai api key from .env
+load_dotenv()
+api_key = os.getenv("OPENAI_API_KEY")
+
+chat = ChatOpenAI(openai_api_key=api_key)
+
+answer = chat.invoke("What is the population of San Fransico?")
+print(answer)
+
